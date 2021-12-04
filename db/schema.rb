061b7e306d7c3b411a0ee817cc12ca344c9ec74c
Diego <diego.edo.valdes@gmail.com>
2021-12-04 19:33:23 -0300

    fix: routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_221626) do
+ActiveRecord::Schema.define(version: 2021_12_04_222621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,18 @@ ActiveRecord::Schema.define(version: 2021_12_04_221626) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "category"
+    t.integer "currency"
+    t.float "price"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_products_on_order_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,4 +105,5 @@ ActiveRecord::Schema.define(version: 2021_12_04_221626) do
   add_foreign_key "contacts", "clients"
   add_foreign_key "orders", "clients"
   add_foreign_key "orders", "users"
+  add_foreign_key "products", "orders"
 end
