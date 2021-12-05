0da5caf3c345bf0efda4bf7afb235b02b4fb4d00
Diego <diego.edo.valdes@gmail.com>
2021-12-05 18:49:54 -0300

    add: add name attribute to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@systemx.cl', password: '123123', password_confirmation: '123123') if Rails.env.development?
+
+
+require 'faker'
+
+User.create([{
+     name: "Diego",
+     email: "dvaldes@test.cl",
+     password: "123456",
+     password_confirmation: "123456",
+     title: "",
+},{
+     name: "Isidora",
+     email: "isidora@test.cl",
+     password: "123456",
+     password_confirmation: "123456"
+},{
+    username: "Valeria",
+    email: "valeria@test.cl",
+    password: "123456",
+    password_confirmation: "123456"
+}])
