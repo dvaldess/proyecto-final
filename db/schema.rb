899f3b91aa75aecd36940d293cb105b6bcc2c1ef
Diego <diego.edo.valdes@gmail.com>
2021-12-05 21:36:58 -0300

    fix: Add Again Status column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_214910) do
+ActiveRecord::Schema.define(version: 2021_12_06_003257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_214910) do
     t.integer "title"
     t.integer "phone"
     t.string "name"
+    t.boolean "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
